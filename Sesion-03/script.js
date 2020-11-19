cd2dfe656045d8bb53aabea1d21da87015966eea
roxolea5 <roxana.olea@gmail.com>
2020-11-19 16:06:14 -0600

    Session 3 challenges completed

--- Sesion-03/script.js
+++ Sesion-03/script.js
@@ -47,3 +47,47 @@ console.log(yearsUntilRetirement(1995, 'John Doe'));
 console.log(whatDoYouDo('developer', 'Diego Mendoza'));
 console.log(whatDoYouDo('designer', 'Aura Leyva'));
 console.log(whatDoYouDo('qa', 'Roxana Olea'));
+
+function power(base, exponent) {
+    console.log('Reto 1');
+    var result = 1;
+  
+    for(var i = 0; i < exponent; i++) {
+      result *= base; // Same as: result = result * base;
+    }
+  
+    return result;
+  }
+
+pot = power(3,3);
+console.log(pot);
+
+
+function getLargerInt(num1, num2) {
+    console.log('Reto 2')
+    if(num1 > num2){
+        return num1;
+    }else{
+        return num2;
+    }
+  }
+
+largest = getLargerInt(14,16);
+console.log(largest);
+
+function fibonacci(num) {
+    if (num <= 1) return 1;
+  
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+  
+  function fibonacciSequence(limit) {
+      console.log('Reto 3');
+    if(limit < 1) return console.log('Limit must be greater than 0');
+  
+    for(var i = 0; i < limit; i++ ) {
+      console.log( fibonacci(i) );
+    }
+  }
+
+  fibonacciSequence(8);
